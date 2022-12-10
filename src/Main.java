a30e8bfa9148f7dd38e713c553d0d3a1acd5732e
madabu <madalinaalbuag@gmail.com>
2022-12-10 11:20:25 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String cnp = "1234567891239";
+
+
+        if (cnp.length() != 13){
+            System.out.println("Nu este CNP.");
+        }
+
+
+
+
+
+
+
+    }
+}
